6c31b4ff24b3d18d3aa16c325d987645e1d48b0b
[forkacper] <[kacperformas@gmail.com]>
2021-10-06 22:40:01 +0200

    strumienie 02-czym jest wejscie, wyjscie, strumien

---
+++ Java Strumienie/02-Czym jest wejscie, wyjscie, strumien/src/Main.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author kacper-test
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // tutaj sama teoria
+        // czytanie -- input dane na wejscie (do drzwi potokiem) -- dane plyna z pliku do zmiennych -- program przetwarzanie
+        // pisanie -- dane na wyjscie (na zewnatrz) OUTPUT -- dane plyna z zmiennych do pliku -- program przetwarzanie
+    }
+    
+}
